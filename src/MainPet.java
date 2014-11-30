57caf8739bf36ffbd13670b9b2f8becf962c16cb
parthtara <tarapaarth@gmail.com>
2014-11-30 14:48:54 -0500

    Nothing

--- src/MainPet.java
+++ src/MainPet.java
@@ -1,7 +1,4 @@
 //This is our main platform to use these classes
 public class MainPet {
-	//This was made by an eleven-year old!
-	//Literally...
-	//Try to use these classes
-	//There might be some issues but you can work with them
+	Dog pet = new Dog();
 }
